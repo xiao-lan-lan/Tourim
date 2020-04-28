b36ae56abe5aa57d8eeb08931f6716182e615940
兰小兰 <479323417@qq.com>
2020-04-28 10:17:08 +0800

    登录&验证码

--- src/api/use.js
+++ src/api/use.js
@@ -5,6 +5,14 @@ export function login (data) {
   return request({
     url: '/user/dologin',
     method: 'post',
-    params: data
+    data: data,
+  })
+}
+
+// 验证码
+export function createValidateCode () {
+  return request({
+    url: '/user/createValidateCode',
+    method: 'get'
   })
 }
